70c73192d23439661227837c16ac0f9c04e72b7f
Henry Pan <legendkiller999@hotmail.com>
2021-10-26 16:53:00 -0400

    Setup MongoDB server

--- COMP229-F2021-MidTerm-301142574/server/config/db.js
+++ COMP229-F2021-MidTerm-301142574/server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://henrypan001:3MQTAhAv0mZveTqT@mongodbserver.34xf5.mongodb.net/books229?retryWrites=true&w=majority"
 };
